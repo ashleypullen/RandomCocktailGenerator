7080449ae90629201f12856eda00ffbc2d355a24
AshleyPullen <85501589+ashleypullen@users.noreply.github.com>
2022-01-23 22:12:34 +0000

    added procfile

--- main.py
+++ main.py
@@ -53,5 +53,5 @@ def randomCocktail():
     return render_template('random.html', cocktail=cocktail, image=image, user=current_user)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
