f44939c63d8f6164c612d4e9dad47c3b2b5e7fe9
AshleyPullen <85501589+ashleypullen@users.noreply.github.com>
2022-01-25 21:20:48 +0000

    moved code from __init__ to main

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 from flask_login import current_user
 from website import create_app
 from flask import Blueprint, render_template
@@ -5,6 +9,42 @@ import requests
 import json
 
 app = create_app()
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'communism'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from website import views
+    from website import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from website import User, Note
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
 
 @app.route('/random')
 def randomCocktail():
@@ -54,5 +94,4 @@ def randomCocktail():
 
 
 if __name__ == "__main__":
-    from os import environ
     app.run(debug=True)
